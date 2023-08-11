82306d655456c70874e8219e404cd65f5f8094d0
meadowlane <nate.gerstner@gmail.com>
2023-08-11 09:10:24 -0700

    Updated search events options

--- all_events.js
+++ all_events.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         // Setup Fuse.js for fuzzy searching
         const options = {
             keys: [
-            { name: 'Event Name', weight: 3 },  // A weight of 2 means it's twice as important
+            { name: 'Event Name', weight: 3 },
             { name: 'Camp Name', weight: 2},
             { name: 'Location', weight: 1},
             { name: 'Description', weight: 1 },
@@ -15,18 +15,18 @@ $(document).ready(function() {
             ],
             threshold: 0.25,
             includeScore: true
-            };
+        };
         const fuse = new Fuse(data, options);
 
         // Setup autocomplete with Fuse.js
         $('#search').autocomplete({
             source: function(request, response) {
                 let results = fuse.search(request.term);
-                let uniqueResults = [...new Set(results.map(item => item.item['Event Name']))];
+                let uniqueResults = [...new Set(results.map(item => `${item.item['Event Name']} (${item.item['Camp Name']} - ${item.item['Location']})`))];
                 response(uniqueResults);
             },
             select: function(event, ui) {
-                displayEventDetails(ui.item.value, data);
+                displayEventDetails(ui.item.value.split(" (")[0], data);  // Extracting only the Event Name from the string
 
                 $('html, body').animate({
                     scrollTop: $("#eventDetails").offset().top - 20
