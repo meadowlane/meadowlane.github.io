be6fe025824d0f36865b538f5196e37bd81a730f
meadowlane <nate.gerstner@gmail.com>
2023-08-14 19:19:27 -0700

    Fixed bug with event names that include paranthesis

--- utils/all_events.js
+++ utils/all_events.js
@@ -17,15 +17,19 @@ $(document).ready(function() {
         $('#search').autocomplete({
             source: function(request, response) {
                 let results = fuse.search(request.term);
-                let uniqueResults = [...new Set(results.map(item => `${item.item['Event Name']} (${item.item['Camp Name']} - ${item.item['Location']})`))];
+                let uniqueResults = [...new Set(results.map(item => {
+                    return {
+                        label: `${item.item['Event Name']} (${item.item['Camp Name']} - ${item.item['Location']})`,
+                        value: item.item['Event Name']
+                    }
+                }))];
                 response(uniqueResults);
             },
             select: function(event, ui) {
-                displayEventDetails(ui.item.value.split(" (")[0], data);
+                displayEventDetails(ui.item.value, data);
                 $('html, body').animate({
                     scrollTop: $("#eventDetails").offset().top - 20
                 }, 300);
-
                 // Blur right away without setTimeout
                 $(this).blur();
             }
@@ -47,28 +51,27 @@ $(document).ready(function() {
             $('#search').val('').trigger('input').focus();
         });
 
-function displayEventDetails(eventName, data) {
-    const event = data.find(e => e['Event Name'] === eventName);
-    if (event) {
-        $('#eventDetails').html(`
-        <div class="card-body">
-                    <h5>Event: ${event['Event Name']}</h5>
-                    <h5>Camp: ${event['Camp Name']}</h5>
-                    <h5>Location: ${event['Location']}</h5>
-                    <br>
-                    <h6>Dates and Times:</h6>
-                    <p>${event['Date and Time'].join('<br>')}</p>
-                    <h6>Type:</h6>
-                    <p>${event['Type']}</p>
-                    <h6>Located at Camp:</h6>
-                    <p>${event["Located at Camp"]}</p>
-                    <h6>Description:</h6>
-                    <p>${event['Description']}</p>
-                    <br>
-                    <a href="${event['Full Details']}" target="_blank">Full Details</a>
-        </div>`);
-    }
-}
-
+        function displayEventDetails(eventName, data) {
+            const event = data.find(e => e['Event Name'] === eventName);
+            if (event) {
+                $('#eventDetails').html(`
+                    <div class="card-body">
+                        <h5>Event: ${event['Event Name']}</h5>
+                        <h5>Camp: ${event['Camp Name']}</h5>
+                        <h5>Location: ${event['Location']}</h5>
+                        <br>
+                        <h6>Dates and Times:</h6>
+                        <p>${event['Date and Time'].join('<br>')}</p>
+                        <h6>Type:</h6>
+                        <p>${event['Type']}</p>
+                        <h6>Located at Camp:</h6>
+                        <p>${event["Located at Camp"]}</p>
+                        <h6>Description:</h6>
+                        <p>${event['Description']}</p>
+                        <br>
+                        <a href="${event['Full Details']}" target="_blank">Full Details</a>
+                    </div>`);
+            }
+        }
     });
 });
